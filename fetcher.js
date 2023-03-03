015318a4e848fc2be341abdb5a217252fd9f45bc
NicolaMGH <va3nky@hotmail.com>
2023-03-03 01:19:06 -0500

    implemented all edge cases

--- fetcher.js
+++ fetcher.js
@@ -1,22 +1,68 @@
 //http://www.example.edu/ ./index.html
 const url = process.argv.slice(2)[0];
 const saveFile = process.argv.slice(3)[0];
+const readline = require('readline');
+const path = require('path');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const fs = require('fs');
 
 const request = require('request');
 request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML
-  fs.writeFile(saveFile, body, err => {
-    if (err) {
-      console.error(err);
-    }
-    fs.stat(saveFile, (err, stats) => {
-      let fileSize = stats.size
-      console.log(`Downloaded and saved ${fileSize} bytes to ${saveFile}`);
+  if (error == null) {
+    //checks to see if this is a valid file path
+    fs.access(path.dirname(saveFile), fs.R_OK, (err) => {
+      if (err == null) {
+        //if path is valid, continue
+        fs.stat(saveFile , (err, stat) => {
+          if (err == null) {
+            //if file exists
+            rl.question('File exists, would you like to overwrite it? Type Y to overwrite or any key to exit. ', (answer) => {
+              if (answer.toLocaleLowerCase() === 'y') {
+                fs.writeFile(saveFile, body, err => {
+                  if (err) {
+                    console.error(err);
+                  }
+                  fs.stat(saveFile, (err, stats) => {
+                    let fileSize = stats.size
+                    console.log(`Downloaded and saved ${fileSize} bytes to ${saveFile}`);
+      
+                    rl.close();
+                  });
+                });
+              } else {
+                //if you do not want to overwrite file
+                rl.close();
+              }
+            });
+          } else if (err.code === 'ENOENT') {
+            // file does not exist
+            fs.writeFile(saveFile, body, err => {
+              if (err) {
+                console.error(err);
+              }
+              fs.stat(saveFile, (err, stats) => {
+                let fileSize = stats.size
+                console.log(`Downloaded and saved ${fileSize} bytes to ${saveFile}`);
+              });
+              rl.close();
+            });
+          }
+        });
+      } else if (err.code === 'ENOENT') {
+        //if path is not valid
+        console.log(`${path.dirname(saveFile)} is not a valid file path`);
+
+        rl.close();
+      }
     });
-    //console.log(`Downloaded and saved ${fileSize} bytes to ${saveFile}`);
-  });
+  } else if (error.code === 'ENOTFOUND') {
+    //if url is not valid
+    console.log(`${url} is not a valid URL`);
+    rl.close();
+  }
 });
